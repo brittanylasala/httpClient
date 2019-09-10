3da74708793090f485e657b72dc90a010bd648ec
Brittany Lasala <brittanylasala@gmail.com>
2019-09-10 20:28:01 +0000

    starting to get modular can be passed all sorts of URL

---
+++ getAndPrintAnyHtml.js
@@ -0,0 +1,31 @@
+// while https is built-in to Node, it is a module, so it must be required
+var https = require('https'); 
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+function getAndPrintHTML (options) {
+
+  let fullHtml = '';
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(options, function (response) {
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      fullHtml += data;
+    });
+  
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(fullHtml);
+    });
+  });
+};
+
+getAndPrintHTML(requestOptions);
